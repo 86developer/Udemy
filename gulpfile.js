35a40dbf538598f8a9bab5ee3e5766113e62a69e
86developer <nunezspinelli8@gmail.com>
2023-05-05 10:45:02 -0300

    sourcemap js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, parallel} = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 
 //CSS
 const sass = require("gulp-sass")(require("sass"));
@@ -16,20 +16,20 @@ const avif = require('gulp-avif');
 
 //Javascript
 const terser = require('gulp-terser-js');
-const beautify = require('gulp-beautify');
-
+/* const beautify = require('gulp-beautify');
+ */
 //Funciones
 function css(done) {    //*Funcion que permite compilar y guardar 
     src("src/scss/**/*.scss")//Identifica todos los archivos en la carpeta
         .pipe(sourcemaps.init()) //Identifica el archivo scss a compilar
-        .pipe( plumber())   //Permite visualizar un error sin cortar el workflow y poder seguir compilando
-        .pipe( sass() )     //Compilar
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe(sourcemaps.write('../maps')) // Antes de guardarlo escribe y lo guarda en la misma carpeta de build
-        .pipe( dest("build/css")) //Almacenarla en destino 
-        .on('end', done);
-
-    //done(); //Callback que avisa a gulp cuando llegamos al final
+        .pipe(plumber())   //Permite visualizar un error sin cortar el workflow y poder seguir compilando
+        .pipe(sass())     //Compilar
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.')) // Antes de guardarlo escribe y lo guarda en la misma carpeta de build
+        .pipe(dest("build/css")) //Almacenarla en destino 
+    /*         .on('end', done);
+     */
+    done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
 function imagenes(done) {
@@ -37,33 +37,34 @@ function imagenes(done) {
         optimizationLevel: 3    //Opciones que viene en la libreria para optimizar con imagemin
     };
     src('src/img/**/*.{png,jpg}')
-        .pipe( cache ( imagemin(opciones) ) )
-        .pipe( dest('build/img') )
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
     done();
 }
-function versionWebp( done ) {  
+function versionWebp(done) {
     const opciones = {
         quality: 50 //Da calidad a la imagen del 1/100
     };
     src('src/img/**/*.{png,jpg}')   //Busca los archivos en destino con los formatos dentro de{}
-        .pipe( webp(opciones) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
     done();
 }
-function versionAvif( done ) {
+function versionAvif(done) {
     const opciones = {
         quality: 50 //Da calidad a la imagen del 1/100
     };
     src('src/img/**/*.{png,jpg}')   //Busca los archivos en destino con los formatos dentro de{}
-        .pipe( avif(opciones) )
-        .pipe( dest('build/img') )
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
     done();
 }
 
-function javascript( done ) {
+function javascript(done) {
     src('src/js/**/*.js')
-        .pipe(beautify({indent_size: 2}))
+        .pipe(sourcemaps.init())
         .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
@@ -82,4 +83,4 @@ exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev);  //Ejecuta en paralelo las funciones indicadas
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);  //Ejecuta en paralelo las funciones indicadas
